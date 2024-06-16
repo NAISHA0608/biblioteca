c0a494e55bbd1b85fc3a51c8f7f503a7307a40f2
NAISHA0608 <naishaayovi00@gmail.com>
2024-06-16 12:21:52 -0500

    qq

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/Sistema_de_biblioteca/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "biblioteca_mvc";
+const charset = "charset=utf8";
+?>
